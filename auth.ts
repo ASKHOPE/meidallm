60d77aafdea1f34fd4d868421ec0b303bd17e403
Arnold Sujan Katru <96995931+ASKHOPE@users.noreply.github.com>
2026-06-24 02:25:38 +0530

    vercel build

--- auth.ts
+++ auth.ts
@@ -1,12 +1,29 @@
 import { betterAuth } from "better-auth";
 import { Pool } from "pg";
+import { dash } from "@better-auth/infra";
 
-const database = new Pool({
-  connectionString: "postgresql://postgres:password@localhost:5432/database",
+const databaseUrl = process.env.DATABASE_URL;
+if (!databaseUrl) {
+  console.error("❌ ERROR: DATABASE_URL is not set in your .env file!");
+  process.exit(1);
+}
+
+// Initialize PG Connection Pool
+const dbPool = new Pool({
+  connectionString: databaseUrl,
+  ssl: {
+    rejectUnauthorized: false // Required for Aiven serverless DB connections
+  }
 });
 
+// Configure Better Auth options
 export const auth = betterAuth({
-  database: database,
-  baseURL: "http://localhost:3000/",
-  emailAndPassword: { enabled: true },
+  database: dbPool,
+  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+  emailAndPassword: {
+    enabled: true
+  },
+  plugins: [
+    dash()
+  ]
 });
